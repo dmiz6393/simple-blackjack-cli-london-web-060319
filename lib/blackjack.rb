f7e2cd39674e0f11f84d0e0912e70ac636c08f28
Danielle Mizrachi <dmiz@brandeis.edu>
2019-04-26 12:35:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,8 +28,12 @@ def initial_round
   return card_total
 end
 
-def hit?(number)
-  # code hit? here
+def hit?(card_total)
+  prompt_user
+  input=get_user_input
+  if input=='h'
+    deal_card
+  if input=='s'
 end
 
 def invalid_command
